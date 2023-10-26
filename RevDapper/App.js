02471e5df6451d54feb4c013825b02405d9f4834
liamgilbert1 <gilbert.li@northeastern.edu>
2023-10-26 18:48:03 -0400

    Merge branch 'Login' of https://github.com/Drtygor/RevDapper into Login

--- RevDapper/App.js
+++ RevDapper/App.js
@@ -1,56 +1,125 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, Dimensions } from 'react-native';
 import LoadingPage from './loadingPage';
 import LoginPage from './loginPage';
 import HomePage from './homePage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
 const Stack = createNativeStackNavigator();
+const numColumns = 2;
 
+const MyFlatList = () => {
+  const recentImages = [
+  { id: '1', imageUri: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F61%2F70%2F61708115ff10932bd9bb771f3110089435ac345e.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_shortsleeve%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/main" },
+    
+  { id: '2', imageUri: "https://www.collinsdictionary.com/images/full/tshirt_204029461_1000.jpg" },
+    
+  { id: '3', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
 
-export default function App() {
-  //list of url
-  const data = [
-    "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F61%2F70%2F61708115ff10932bd9bb771f3110089435ac345e.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_shortsleeve%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/main",
-  
-  
-  ]
-  
-  return (
-<<<<<<< Updated upstream
-    <NavigationContainer>
-      <Stack.Navigator>
+  { id: '4', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
 
-    <Stack.Screen name="Login" component={LoginPage} />    
-    <Stack.Screen name="HomePage" component={HomePage} />
+  { id: '5', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
 
+  { id: '6', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
 
-    </Stack.Navigator>
-    </NavigationContainer>
+  { id: '7', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
+
+  { id: '8', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
+
+  { id: '9', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
+
+  { id: '10', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
+
+  { id: '11', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
+
+  { id: '12', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
+
+  { id: '13', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" },
+
+  { id: '14', imageUri: "https://media.istockphoto.com/id/697913308/de/foto/orange-herren-shorts-zum-baden.jpg?s=612x612&w=0&k=20&c=DPJH9bXBxld4AYMxkfF_cATVRg5X4NSfCdlcuUmP_C0=" }
+
+    
+  ];
+      
+  const renderItem = ({ item }) => (
+    <Image source={{ uri: item.imageUri }} style={appStyles.image} />
   );
-}
+      
+  return (
+    <View style={appStyles.container}>
+      <Text style={appStyles.recentlyUploadedText}>Recently Uploaded</Text>
+      <FlatList
+        data={recentImages}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        horizontal={false}
+        numColumns={numColumns}
 
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'gray', 
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-  },
-});
-=======
-    
-
-    <View>
-      // text: recently uploaded
-      // scrollview
-      <image></image> // find out how to render url to image
-      // dynamically render all images
+      />
     </View>
   );
-}
->>>>>>> Stashed changes
+};
+      
+const appStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  recentlyUploadedText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 10,
+    paddingTop: 40,
+    paddingBottom: 10,
+    color: 'white',
+    backgroundColor: 'black',
+    padding: 10
+  },
+  image: {
+    width: 150,
+    height: 150,
+    margin: 10,
+  },
+ });
+      
+  const App = () => {
+    return (
+      <View style={styles.appContainer}>
+        <MyFlatList />
+      </View>
+      
+    );
+  };
+      
+  const styles = StyleSheet.create({
+    appContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'black'
+    },
+  });
+
+  export default App;
+ 
+
+
+/*<NavigationContainer>
+<Stack.Navigator>
+
+<Stack.Screen name="Login" component={LoginPage} />    
+<Stack.Screen name="HomePage" component={HomePage} />
+
+
+</Stack.Navigator>
+</NavigationContainer> */
+
+
+
+
+    
+
+    
+
