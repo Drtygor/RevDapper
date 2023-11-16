6e19cbf84c50d03ead40dd28ae244eb7006b6bef
Drtygor <kyeung114@gmail.com>
2023-11-16 00:25:43 -0500

    favorites screen
    Bruh navigate not working

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,127 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import profile from '../assets/blank-profile-picture-973460_1280.png'
-import Logo from '../assets/FinalDapperLogo.png'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import profile from "../assets/blank-profile-picture-973460_1280.png";
+import Logo from "../assets/FinalDapperLogo.png";
 
 //Handles Logout Functionality
 const handleLogout = (navigation) => {
-    navigation.navigate('Landing');
+  navigation.navigate("Landing");
 };
 
 //Handles the setting navigation
 const handleSetting = (navigation) => {
-    navigation.navigate('Settings');
-}
+  navigation.navigate("Settings");
+};
 
-const Profile = ({navigation}) => {
-    return (
-        <View style={styles.container}>
-            <StatusBar style="dark" />
+//Handles the favorites navigation
+const handleFavorites = (navigation) => {
+  navigation.navigate("Favorites");
+};
 
-             {/*The Dapper Logo */}
-             <View style = {styles.logoStyle}>
-                <Image
-                source = {Logo}
-                style = {styles.logoImage}
-                />
-            </View>
+const Profile = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="dark" />
 
-            {/*The profile icon */}
-            <View style = {styles.profile}>
-                <Image
-                source = {profile}
-                style = {styles.profileImage}
-                />
-            </View>
+      {/*The Dapper Logo */}
+      <View style={styles.logoStyle}>
+        <Image source={Logo} style={styles.logoImage} />
+      </View>
 
-            {/*The Dummy Username */}
-            <View style = {styles.usernameContainer}>
-                <Text style = {styles.textFormat}>Username</Text>
-            </View>
+      {/*The profile icon */}
+      <View style={styles.profile}>
+        <Image source={profile} style={styles.profileImage} />
+      </View>
 
-            {/*Logout Button*/}
-            <TouchableOpacity 
-            style = {styles.settingButtons}
-            onPress = {() => handleLogout(navigation)}>
-                <Text style = {styles.textFormat}>Logout</Text>
-            </TouchableOpacity>
+      {/*The Dummy Username */}
+      <View style={styles.usernameContainer}>
+        <Text style={styles.textFormat}>Username</Text>
+      </View>
 
-            {/*Settings Page Button*/}
-            <TouchableOpacity
-            style = {styles.settingButtons}
-            onPress = {() => handleSetting(navigation)}>
-                <Text style = {styles.textFormat}>Settings</Text>
-            </TouchableOpacity>
+      {/*Logout Button*/}
+      <TouchableOpacity
+        style={styles.settingButtons}
+        onPress={() => handleLogout(navigation)}
+      >
+        <Text style={styles.textFormat}>Logout</Text>
+      </TouchableOpacity>
 
-             {/*About Page Button*/}
-             <TouchableOpacity
-            style = {styles.settingButtons}
-            >
-                <Text style = {styles.textFormat}>About</Text>
-            </TouchableOpacity>
+      {/*Favorites Page Button*/}
+      <TouchableOpacity
+        style={styles.settingButtons}
+        onPress={() => handleFavorites(navigation)}
+      >
+        <Text style={styles.textFormat}>Favorites</Text>
+      </TouchableOpacity>
 
-        </View>
+      {/*Settings Page Button*/}
+      <TouchableOpacity
+        style={styles.settingButtons}
+        onPress={() => handleSetting(navigation)}
+      >
+        <Text style={styles.textFormat}>Settings</Text>
+      </TouchableOpacity>
 
-    );
+      {/*About Page Button*/}
+      <TouchableOpacity style={styles.settingButtons}>
+        <Text style={styles.textFormat}>About</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 //The styles/formatting for everything on the profile page
 const styles = StyleSheet.create({
-    container : {
-        flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center',
-        backgroundColor : 'black'
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
 
-    logoStyle : {
-       alignItems : 'center',
-       justifyContent : 'center'
-    },
+  logoStyle: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    logoImage : {
-        width : 150,
-        height : 150
-    },
+  logoImage: {
+    width: 150,
+    height: 150,
+  },
 
-    profile : {
-        flexDirection : 'column',
-        flex : 1,
-        alignItems: 'center',
+  profile: {
+    flexDirection: "column",
+    flex: 1,
+    alignItems: "center",
+  },
 
-    },
+  profileImage: {
+    width: 100,
+    height: 100,
+  },
 
-    profileImage : {
-        width : 100,
-        height : 100
-    },
+  usernameContainer: {
+    alignItems: "center",
+    backgroundColor: "gray",
+    borderRadius: 10,
+    justifyContent: "center",
+    padding: 10,
+    width: "50%",
+    marginBottom: "20%",
+    flexDirection: "column",
+  },
 
-    usernameContainer : {
-        alignItems : 'center',
-        backgroundColor : 'gray',
-        borderRadius : 10,
-        justifyContent : 'center',
-        padding : 10,
-        width : '50%',
-        marginBottom : "20%",
-        flexDirection : 'column'
-    },
+  textFormat: {
+    fontSize: 25,
+    textAlign: "center",
+    fontWeight: "normal",
+    color: "black",
+  },
 
-    textFormat : {
-        fontSize : 25,
-        textAlign : 'center',
-        fontWeight : 'normal',
-        color : 'black',
-    },
-
-    settingButtons : {
-        backgroundColor: 'white',
-        padding: 10,
-        borderRadius: 10,
-        width: "50%",
-        flexDirection : 'column',
-        marginBottom : 30,
-    }
-
+  settingButtons: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    width: "50%",
+    flexDirection: "column",
+    marginBottom: 30,
+  },
 });
-export default Profile
+export default Profile;
